2516c5c8893e965125c748637078a7ad76b9158e
matvlaale <maculaali@yandex.ru>
2020-06-22 12:15:52 +0300

    Lesson_2

--- server/src/main/java/server/AuthService.java
+++ server/src/main/java/server/AuthService.java
@@ -1,6 +1,11 @@
 package server;
 
+import java.sql.SQLException;
+
 public interface AuthService {
+    void connect() throws ClassNotFoundException, SQLException;
+    void disconnect();
     String getNicknameByLoginAndPassword(String login, String password);
     boolean registration(String login, String password, String nickname);
+    void changeNick(ClientHandler ch, String password, String newNick);
 }
--- server/src/main/java/server/SimpleAuthService.java
+++ server/src/main/java/server/SimpleAuthService.java
@@ -1,9 +1,30 @@
 package server;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleAuthService implements AuthService {
+
+    private static Connection connection;
+    public static Statement stmt;
+
+    @Override
+    public void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection = DriverManager.getConnection("jdbc:sqlite:server.db");
+        stmt = connection.createStatement();
+    }
+    @Override
+    public void disconnect(){
+        try{
+            stmt.close();
+            connection.close();
+            System.out.println("DataBase closed");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
     private class UserData{
         String login;
@@ -19,8 +40,30 @@ public class SimpleAuthService implements AuthService {
 
     private List<UserData> users;
 
+    private void loadData(){
+        try {
+            ResultSet data = stmt.executeQuery("SELECT * FROM users");
+            while(data.next()){
+                UserData newUD = new UserData(data.getString("login"),
+                        data.getString("password"), data.getString("nick"));
+                users.add(newUD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public SimpleAuthService() {
         this.users = new ArrayList<>();
+
+        try {
+            connect();
+            System.out.println("Connected");
+            loadData();
+            System.out.println("Loaded");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         for (int i = 1; i <= 10 ; i++) {
             users.add(new UserData("login"+i, "pass"+i, "nick"+i));
@@ -41,6 +84,22 @@ public class SimpleAuthService implements AuthService {
 
         return null;
     }
+    @Override
+    public void changeNick(ClientHandler ch, String password, String newNick){
+        String login = ch.getLogin();
+        for (UserData o:users ) {
+            if(o.login.equals(login) && o.password.equals(password)){
+                o.nickname = newNick;
+            }
+        }
+        try {
+            stmt.executeUpdate(String.format("UPDATE users SET nick = '%s' " +
+                    "WHERE login = '%s' AND password = '%s';", newNick, login, password));
+            System.out.println("Nick changed");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public boolean registration(String login, String password, String nickname) {
@@ -50,6 +109,13 @@ public class SimpleAuthService implements AuthService {
             }
         }
         users.add(new UserData(login, password, nickname));
+        try {
+            stmt.executeUpdate(String.format("INSERT INTO users (login, password, nick) " +
+                    "VALUES ('%s', '%s', '%s');", login, password, nickname));
+            System.out.println("Registered");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return true;
     }
 }
